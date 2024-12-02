026e47e39bb79461216a6182691844f79e3fb6ee
ziparh <thelackutop@gmail.com>
2024-12-02 21:41:44 +0100

    add naming conventions

--- app/core/db/base.py
+++ app/core/db/base.py
@@ -1,7 +1,15 @@
+from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr
+
+from app.core import settings
 from app.utils.case_conventer import camel_case_to_snake_case
 
+
 class Base(DeclarativeBase):
+    __abstract__ = True
+
+    metadata = MetaData(naming_convention=settings.db.naming_convention)
+
     @declared_attr.directive
     def __tablename__(cls) -> str:
         return f'{camel_case_to_snake_case(cls.__name__)}s'
