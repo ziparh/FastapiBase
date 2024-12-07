7633efe405bb63d1200468e22acfc2e6bdcfcdbd
ziparh <thelackutop@gmail.com>
2024-12-07 16:00:01 +0100

    refactor architecture

--- app/core/db/base.py
+++ app/core/db/base.py
@@ -2,7 +2,7 @@ from sqlalchemy import MetaData
 from sqlalchemy.orm import DeclarativeBase, declared_attr
 
 from app.core import settings
-from app.utils.case_conventer import camel_case_to_snake_case
+from app.core.utils import camel_case_to_snake_case
 
 
 class Base(DeclarativeBase):
